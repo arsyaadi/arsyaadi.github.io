98ef947781529d5e3b4724bd0acc3ad978f0438a
arsyaadi <arsyaadi@gmail.com>
2021-12-26 20:51:49 +0700

    initial master

---
+++ bootstrap-4.1.3-dist/js/script.js
@@ -0,0 +1,14 @@
+// const navItem = document.querySelectorAll(".nav-link")
+
+
+// navItem.forEach(e => {
+//     e.addEventListener('click',function(){
+//         navItem.classList.toggle('active')
+//         // console.log('ok')
+//     })
+// });
+
+// $(".nav .nav-link").on("click", function(){
+//     $(".nav").find(".active").removeClass("active");
+//     $(this).addClass("active");
+//  });
