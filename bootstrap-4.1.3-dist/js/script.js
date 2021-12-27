f009dcf6d8c819e656e7ddf86868fd983b52f13f
arsyaadi <arsyaadi@gmail.com>
2021-12-27 20:35:44 +0700

    menambahkan class active jika navbar dipencet

--- bootstrap-4.1.3-dist/js/script.js
+++ bootstrap-4.1.3-dist/js/script.js
@@ -1,14 +1,14 @@
-// const navItem = document.querySelectorAll(".nav-link")
+const navItem = document.querySelectorAll(".nav-link")
 
 
-// navItem.forEach(e => {
-//     e.addEventListener('click',function(){
-//         navItem.classList.toggle('active')
-//         // console.log('ok')
-//     })
-// });
-
-// $(".nav .nav-link").on("click", function(){
-//     $(".nav").find(".active").removeClass("active");
-//     $(this).addClass("active");
-//  });
+navItem.forEach(el => {
+    el.addEventListener('click',function(e){
+        navItem.forEach(el => {
+            if(el.classList.contains('active')){
+                el.classList.remove('active')
+            }
+        })
+        e.target.classList.toggle('active')
+        // console.log('ok')
+    })
+});
